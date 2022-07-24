2b22751b637d1b1e13c4a74eb1e3cf73faf09830
OrangeCat <75605629+OrangeCat2528@users.noreply.github.com>
2022-07-24 17:04:20 +0700

    Update Papan.tsx

--- src/components/Papan.tsx
+++ src/components/Papan.tsx
@@ -6,12 +6,12 @@ const Papan: FC = () => {
   const name = import.meta.env.VITE_APP_NAME
 
   const now = moment().locale('id')
-  const that = moment('2004-06-22').locale('id')
+  const that = moment('2008-02-25').locale('id')
   const birthday = that.date() === now.date() && that.month() === now.month()
 
   const description = birthday
-    ? 'Heyy, hari ini ulang tahun akuu lohh... mauu ngucapin apa cobaa sekarang?? 😝'
-    : 'Ketikin apapun yang kamu pengen sampein ke aku.. tapi inget yaa, gaboleh nyeleneh lohh 🥰'
+    ? 'Hai Kamu! Hari ini farrel Sedang loh! Mau Ngucapin?'
+    : 'Ketikin apapun yang kamu pengen sampein ke aku.. Tapi ingat, jangan Yang Aneh" ya 🥰'
 
   return (
     <div className="papan">
