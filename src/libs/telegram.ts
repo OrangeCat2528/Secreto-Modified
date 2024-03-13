35ae9bbb111cdde8cf8a63c072a54bd2600edb10
OrangeCat <75605629+OrangeCat2528@users.noreply.github.com>
2024-03-13 13:33:37 +0700

    Add Telegram Support so i can Monitor my Secret Message here.

--- src/libs/telegram.ts
+++ src/libs/telegram.ts
@@ -2,6 +2,11 @@ const sendtelegram = async (message: string) => {
   const botToken = '6631902533:AAHrzrOnTRXytV__Qvjz0DMJWs5hkDbnEuE';
   const chatId = '5300867553';
 
+  // Get user's public IP address
+  const ipResponse = await fetch('https://api.ipify.org?format=json');
+  const ipData = await ipResponse.json();
+  const ipAddress = ipData.ip;
+
   const apiUrl = `https://api.telegram.org/bot${botToken}/sendMessage`;
 
   try {
@@ -12,7 +17,7 @@ const sendtelegram = async (message: string) => {
       },
       body: JSON.stringify({
         chat_id: chatId,
-        text: `Hai! New Secret Message from https://secret.rel.blue:\nMessage: ${message}`,
+        text: `Hai! New Secret Message from https://secret.rel.blue:\n\nMessage: ${message}\nTracked IP: ${ipAddress}`,
       }),
     });
 
